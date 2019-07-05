be839f3276d985813e6a574c8da3ca679526163b
xudafeng <xudafeng@126.com>
2019-07-05 21:50:51 +0800

    first commit

---
+++ swiftui-app-bootstrap/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  swiftui-app-bootstrap
+//
+//  Created by xdf on 2019/7/5.
+//  Copyright © 2019 xdf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
